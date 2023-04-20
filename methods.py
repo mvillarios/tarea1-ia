7a654c2ee78a850fd140457bd2ccea673ff38b06
miosv <mvilla2019@inf.udec.cl>
2023-04-19 22:57:33 -0400

    "Metodos implementados" revisar si estan bien*

--- methods.py
+++ methods.py
@@ -1,37 +1,37 @@
 import random
 
-def DFS(init, goal, nodes, graph):
-    stack = []
-    path = []
-    expand_count = {node: 0 for node in nodes}
-    total_cost = 0
+# def DFS(init, goal, nodes, graph):
+#     stack = []
+#     path = []
+#     expand_count = {node: 0 for node in nodes}
+#     total_cost = 0
 
-    node = init
-    stack.append(node)
+#     node = init
+#     stack.append(node)
     
-    while stack: # Mientras el stack no este vacio (hay nodos por expandir)
+#     while stack: # Mientras el stack no este vacio (hay nodos por expandir)
 
-        # El nodo que se elige del stack debe ser uno de los hijos del anterior
-        num = random.randint(0, len(stack)-1)
-        while stack[num] != init and stack[num] not in graph[node]:
-            num = random.randint(0, len(stack)-1)
+#         # El nodo que se elige del stack debe ser uno de los hijos del anterior
+#         num = random.randint(0, len(stack)-1)
+#         while stack[num] != init and stack[num] not in graph[node]:
+#             num = random.randint(0, len(stack)-1)
         
-        node = stack.pop(num)
+#         node = stack.pop(num)
         
-        if node == goal:
-            path.append(node)
+#         if node == goal:
+#             path.append(node)
 
-            # Costo total del camino
-            total_cost = sum(graph[path[i]][path[i+1]] for i in range(len(path)-1))
+#             # Costo total del camino
+#             total_cost = sum(graph[path[i]][path[i+1]] for i in range(len(path)-1))
 
-            return path, total_cost, expand_count
+#             return path, total_cost, expand_count
         
-        if node not in path:
-            path.append(node)
-            expand_count[node] += 1
+#         if node not in path:
+#             path.append(node)
+#             expand_count[node] += 1
 
-            for child in graph[node]:
-                stack.append(child)
+#             for child in graph[node]:
+#                 stack.append(child)
     
 
 # def a_estrella(init, goal, nodes, graph):
@@ -72,27 +72,13 @@ def DFS(init, goal, nodes, graph):
 #                 stack.append(child)
 
 
-# def a_estrella(init, goal, nodes, graph):
-#     stack = {} # Va guardando el camino y el costo real del mismo
-#     path = [init]
-#     stack[path] = 0
 
-#     print(stack)
-#     expand_count = {node: 0 for node in nodes}
-#     total_cost = 0
-
-
-    # while True:
-    #     f_min = float('inf')
-    #     node_min = None
-
-    #     for camino in stack:
             
 
             
 def uniform_cost_search(init, goal, nodes, graph):
 
-    lista_nodos = [(init, 0, [init])] # Lista de nodos a expandir guarda el nodo inicial con costo 0 y camino vacio
+    lista_nodos = [(init, 0, [init])] # Lista de nodos a expandir guarda el nodo actual, el costo total, y el camino
 
     expand_count = {node: 0 for node in nodes}
 
@@ -108,6 +94,8 @@ def uniform_cost_search(init, goal, nodes, graph):
         if node == goal:
             return camino, costo, expand_count
 
+
+        expand_count[node] += 1
         for vecino in graph[node]:
             
             costo_total = costo + graph[node][vecino]
@@ -116,11 +104,97 @@ def uniform_cost_search(init, goal, nodes, graph):
 
             lista_nodos.append(nodo_nuevo)
 
-        if expand_count[node] == 0:
-            expand_count[node] = 1
 
     return None
 
 
+def a_estrella(init, goal, nodes, graph):
+    lista_nodos = [(init, 0, [init])] # Lista de nodos a expandir guarda el nodo actual, el costo total, y el camino
+
+    expand_count = {node: 0 for node in nodes}
+
+    #count = 0
+    while lista_nodos:
+
+        # Ordena la lista de nodos segun la funcion f(n) = g(n) + h(n) de menor a mayor
+        lista_nodos.sort(key=lambda x: x[1] + nodes[x[0]])
+        #count += 1
+        #print("Iteracion: ",count)
+        # for nodo in lista_nodos:
+        #     print("Camino: ",nodo[2], " Costo: ",nodo[1] + nodes[nodo[0]])
+        # Obtiene el nodo con menor costo
+        node, costo, camino = lista_nodos.pop(0)
+        
+        #print("Camino tomado: ", camino)
+
+        # Si el nodo es el objetivo, retorna el camino y el costo
+        if node == goal:
+            return camino, costo, expand_count
+
+        expand_count[node] += 1 
+
+        for vecino in graph[node]:
+            
+            costo_total = costo + graph[node][vecino]
+            camino_nuevo = camino.copy() + [vecino]
+            nodo_nuevo = (vecino, costo_total, camino_nuevo)
+
+            lista_nodos.append(nodo_nuevo)
+
+        
+
+    return None
 
 
+def DFS(init, goal, nodes, graph):
+    
+    lista_nodos = [(init, 0, [init])] 
+
+    expand_count = {node: 0 for node in nodes}
+
+    visited = set()
+
+    while lista_nodos:
+
+        node, costo, camino = lista_nodos.pop(random.randint(0, len(lista_nodos)-1))
+
+        if node == goal:
+            return camino, costo, expand_count
+
+        visited.add(node)
+
+        for vecino in graph[node]:
+            if vecino not in visited:
+                costo_total = costo + graph[node][vecino]
+                camino_nuevo = camino.copy() + [vecino]
+                nodo_nuevo = (vecino, costo_total, camino_nuevo)
+                lista_nodos.append(nodo_nuevo)
+                
+
+def greedy(init, goal, nodes, graph):
+
+    lista_nodos = [(init, 0, [init])] # Lista de nodos a expandir guarda el nodo actual, el costo total, y el camino
+
+    expand_count = {node: 0 for node in nodes}
+
+    while lista_nodos:
+
+        # Ordena la lista de nodos por costo
+        lista_nodos.sort(key=lambda x: nodes[x[0]])
+
+        # Obtiene el nodo con menor costo
+        node, costo, camino = lista_nodos.pop(0)
+
+        # Si el nodo es el objetivo, retorna el camino y el costo
+        if node == goal:
+            return camino, costo, expand_count
+
+
+        expand_count[node] += 1
+        for vecino in graph[node]:
+            
+            costo_total = costo + graph[node][vecino]
+            camino_nuevo = camino.copy() + [vecino]
+            nodo_nuevo = (vecino, costo_total, camino_nuevo)
+
+            lista_nodos.append(nodo_nuevo)
